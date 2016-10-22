239d3852bfff2f73df6b8b95b14afcb4034ed1da
James Jenkins <applesmacs@yahoo.com>
2016-10-22 20:33:08 +0000

    Reorder getd includes; Comment safe_sid_copy

--- getd.c
+++ getd.c
@@ -13,8 +13,8 @@
 #include <nanomsg/pair.h>
 
 #include "apue.h"
-#include "message.h"
 #include "jlibc/hashmap/hashmap.h"
+#include "message.h"
 
 
 #define initmsgtype(N) {.header.messageType = N,\
@@ -26,8 +26,10 @@
 
 HashMap *sessions;
 
+
 /*
- *
+ * A safe copy for session ids. Always only use nlength and set a null byte at
+ * the end of the dest.
  */
 void safe_sid_copy(char *dest, char const *src) {
     strncpy(dest, src, SID_LENGTH);
